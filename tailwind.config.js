f2a238d901ac28c480a38c546fff7e277f30d3f6
jf <julian.furchert@raytion.com>
2022-09-11 00:38:32 +0200

    Add image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      primary: '#d9bdb0',
+    },
     extend: {},
   },
   plugins: [],
