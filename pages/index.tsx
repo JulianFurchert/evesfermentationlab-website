a0d20de4682158c18a1613e37e7073b1079a0c12
jf <julian.furchert@raytion.com>
2022-09-15 23:58:01 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,27 @@ import { useScroll, useTransform } from "framer-motion"
 import { useWindowWidth } from '@react-hook/window-size'
 import { useMediaQuery } from 'react-responsive'
 
+const images = [
+  "/assets/img/01_IMG_0971.jpg",
+  "/assets/img/02_IMG_9888.jpg",
+  "/assets/img/03_IMG_6224.jpg",
+  // "/assets/img/04_IMG_9886.jpg",
+  "/assets/img/05_IMG_7519.jpg",
+  "/assets/img/06_IMG_7747.jpg",
+  "/assets/img/07_IMG_8764.jpg",
+  "/assets/img/IMG_6786.jpg",
+  "/assets/img/IMG_6788.jpg",
+  "/assets/img/IMG_6797.jpg",
+  "/assets/img/IMG_6818.jpg",
+  "/assets/img/08_IMG_9882 3.jpg",
+  "/assets/img/09_IMG_9994.jpg",
+  "/assets/img/10_IMG_9916.jpg",
+  "/assets/img/11_IMG_8066.jpg",
+  "/assets/img/12_IMG_9905.jpg",
+  "/assets/img/13_IMG_9868.jpg",
+  "/assets/img/14_IMG_9909.jpg",
+]
+
 const Home: NextPage = () => {
   const isMobil = useMediaQuery({ query: '(max-width: 899px)' })
   const { scrollY  } = useScroll();
@@ -31,12 +52,7 @@ const Home: NextPage = () => {
 
             <div className='w-[100vw] h-[100vw] flex relative justify-center items-center overflow-hidden'>
               <Gallery 
-                images={[
-                  "/assets/img/IMG_6786.jpg",
-                  "/assets/img/IMG_6788.jpg",
-                  "/assets/img/IMG_6797.jpg",
-                  "/assets/img/IMG_6818.jpg",
-                ]}
+                images={images}
               />
             </div>
 
@@ -70,26 +86,7 @@ const Home: NextPage = () => {
             <div className='fixed right-0 top-0 w-[50vw] h-screen flex overflow-hidden bg-primary'>
               <div className='w-[50vw] h-screen flex relative justify-center items-center overflow-hidden'>
                 <Gallery 
-                  images={[
-                    "/assets/img/01_IMG_0971.jpg",
-                    "/assets/img/02_IMG_9888.jpg",
-                    "/assets/img/03_IMG_6224.jpg",
-                    // "/assets/img/04_IMG_9886.jpg",
-                    "/assets/img/05_IMG_7519.jpg",
-                    "/assets/img/06_IMG_7747.jpg",
-                    "/assets/img/07_IMG_8764.jpg",
-                    "/assets/img/IMG_6786.jpg",
-                    "/assets/img/IMG_6788.jpg",
-                    "/assets/img/IMG_6797.jpg",
-                    "/assets/img/IMG_6818.jpg",
-                    "/assets/img/08_IMG_9882 3.jpg",
-                    "/assets/img/09_IMG_9994.jpg",
-                    "/assets/img/10_IMG_9916.jpg",
-                    "/assets/img/11_IMG_8066.jpg",
-                    "/assets/img/12_IMG_9905.jpg",
-                    "/assets/img/13_IMG_9868.jpg",
-                    "/assets/img/14_IMG_9909.jpg",
-                  ]}
+                  images={images}
                 />
               </div>
             </div>
